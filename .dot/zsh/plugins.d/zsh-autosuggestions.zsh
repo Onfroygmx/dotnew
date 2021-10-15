1f93313309e1fa9ddf2146da4e0a2c0af81ceb64
Onf <onfroy@gmx.com>
2021-10-15 15:14:42 +0200

    Add rebind option

--- .dot/zsh/plugins.d/zsh-autosuggestions.zsh
+++ .dot/zsh/plugins.d/zsh-autosuggestions.zsh
@@ -5,3 +5,4 @@ builtin source $PLUGIN_DIR/zsh-users/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Set color of autosuggestions and ignore leading spaces
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=69'
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
